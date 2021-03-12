e541e28867299db2135ca5e0e5f71fb854a18aaf
WonderBiz-hub <80506148+WonderBiz-hub@users.noreply.github.com>
2021-03-12 14:34:57 +0530

    Add files via upload

---
+++ My Certificates Demo/ServiceHostConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+//using WcfServiceLibrary1;
+
+namespace ServiceHostConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is service host");
+
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(SampleWCFServiceLibrary.Service1));
+                host.Open();
+                Console.WriteLine("Service Hosted Sucessfully");
+                Console.Read();
+                host.Close();
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine("Exception :- "+ex.Message.ToString());
+            }
+
+            Console.Read();
+
+            
+
+        }
+    }
+}
